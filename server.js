b378dc2f1b6836f1efecbdaccd42759fde204c3f
wmx <71251055+2509710895@users.noreply.github.com>
2023-03-08 20:42:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const client = new OSS({
   // yourregion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
   region: "oss-cn-chengdu",
   // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-  accessKeyId: "LTAI5tCiuAAjM8J3UtJEZAUy",
-  accessKeySecret: "19raiasjg5ayW9p933D7lxmWCWmXBk",
+  accessKeyId: "LTAI5tCiuAAjM8xxxxxxxxxxxxxxx",
+  accessKeySecret: "19raiasjg5ayW9p933D7xxxxxxxxxxxxxxxxxx",
   // 填写Bucket名称。
   bucket: "wmx-blog",
 });
